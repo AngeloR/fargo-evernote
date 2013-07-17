bba0290101406dd41e9f332d3dea4209d67ae4a7
Angelo R <angelo@airpost.io>
2013-07-17 00:26:26 -0700

    added jquery bindings for click events

--- evernote.js
+++ evernote.js
@@ -7,7 +7,9 @@
  */
 
 var FargoEvernote = {
-	handleClick: function() {
+	handleClick: function(e) {
+		e.preventDefault();
+		e.stopPropagation();
 
 		// grab the selected nodeText
 		var text = getOutlineText();
@@ -19,9 +21,11 @@ var FargoEvernote = {
 	},
 	init: function() {
 		// Add the evernote icon to the sidebar
-		var html = '<div class="divIcon"><a href="#" rel="tooltip" data-placement="left" data-original-title="Send to Evernote" onclick="FargoEvernote.handleClick()">';
+		var html = '<div class="divIcon"><a href="#" rel="tooltip" data-placement="left" data-original-title="Send to Evernote" id="send-to-evernote">';
 			html += '<img src="https://dl.dropboxusercontent.com/s/mk69whgni3qntgk/evernote-black.png?m"></a></div>';
 		$('#idIconChain').append(html);
+
+		$('#idIconChain').on('click', '#send-to-evernote', FargoEvernote.handleClick);
 	}
 };
 
